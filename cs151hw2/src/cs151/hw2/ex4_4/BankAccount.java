12d028a76dd457f6a3fc3d4d01f0ba4a8d5b0af7
jerry_laptop <jiajiewu1988@gmail.com>
2012-09-21 17:12:08 -0700

    Ex. 4.4

---
+++ cs151hw2/src/cs151/hw2/ex4_4/BankAccount.java
@@ -0,0 +1,62 @@
+package cs151.hw2.ex4_4;
+
+/*
+ * A bank account with holder name, account #, and balance
+ */
+public class BankAccount implements Comparable<BankAccount> {
+	private String hName;
+	private String accNo;
+	private double bal;
+	
+	/*
+	 * Constructor to initialize holder name, account #, and balance
+	 * @param name holder name
+	 * @param num account number
+	 * @param bal balance
+	 */
+	public BankAccount(String name, String num, double bal) {
+		hName = name;
+		accNo = num;
+		this.bal = bal;
+	}
+	
+	/*
+	 * Get the bank account holder name
+	 * @return holder name
+	 */
+	public String getName() {
+		return hName;
+	}
+	
+	/*
+	 * Get the account number
+	 * @return account number
+	 */
+	public String getAccNo() {
+		return accNo;
+	}
+	
+	/*
+	 * Get the balance
+	 * @return balance
+	 */
+	public double getBalance() {
+		return bal;
+	}
+	
+	/*
+	 * Compares 2 bank account by balance
+	 * @param other another bank account
+	 * @return a negative number if the balance is less than the balance in other account
+	 * a positive number if the balance is greater than the balance in other account
+	 * return 0 if the balance is the same
+	 */
+	@Override
+	public int compareTo(BankAccount other) {
+		if (bal < other.getBalance()) {
+			return -1;
+		} else if (bal > other.getBalance())
+			return 1;
+		return 0;
+	}
+}
---
+++ cs151hw2/src/cs151/hw2/ex4_4/BankAccountTester.java
@@ -0,0 +1,19 @@
+package cs151.hw2.ex4_4;
+
+import java.util.*;
+
+public class BankAccountTester {
+	public static void main(String args[]) {
+		ArrayList<BankAccount> accList = new ArrayList<BankAccount>();
+		accList.add(new BankAccount("Jerry Wu", "007212361", 1000.23));
+		accList.add(new BankAccount("Kang Ge", "006987789", 1234.5));
+		accList.add(new BankAccount("Bing Ge", "008764536", 421.4));
+		
+		Collections.sort(accList);
+		
+		for (BankAccount a : accList) {
+			System.out.println(a.getName() + " : Balance is " + a.getBalance());
+		}
+	}
+
+}
