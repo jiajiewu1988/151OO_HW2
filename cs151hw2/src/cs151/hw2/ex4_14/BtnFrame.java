883abf7973d1bc6afbe629c4f41aa6a4ea3906da
Jerry-Desktop <jiajiewu1988@gmail.com>
2012-09-25 14:32:09 -0700

    start ex 4.14

---
+++ cs151hw2/src/cs151/hw2/ex4_14/BtnFrame.java
@@ -0,0 +1,7 @@
+package cs151.hw2.ex4_14;
+
+import javax.swing.JFrame;
+
+public class BtnFrame extends JFrame {
+
+}
---
+++ cs151hw2/src/cs151/hw2/ex4_14/BtnPanel.java
@@ -0,0 +1,7 @@
+package cs151.hw2.ex4_14;
+
+import javax.swing.JPanel;
+
+public class BtnPanel extends JPanel{
+
+}
